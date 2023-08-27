ae88a14b43470155b22927bc65024ff89166e3f1
Aniket Teltumbade <teltumbade.s.aniket@gmail.com>
2023-08-28 00:14:48 +0530

    first commit

---
+++ src/Components/AppBar/index.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import DrawerComponent from '../Drawer';
+
+export default function AppBarComponent() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+      <AppBar position="static">
+        <Toolbar>
+         <DrawerComponent/>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Admin
+          </Typography>
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+        </Toolbar>
+      </AppBar>
+  );
+}
---
+++ src/Utils/appbarMenus.ts
@@ -0,0 +1,16 @@
+import Dashboard from '@mui/icons-material/Dashboard';
+import Inventory from '@mui/icons-material/Inventory';
+import School from '@mui/icons-material/School';
+import Category from '@mui/icons-material/Category';
+import Business from '@mui/icons-material/Business';
+import Shop2 from '@mui/icons-material/Shop2';
+
+const appbarMenus =[
+    {code: 'overview', title : 'Dashboard', icon: Dashboard},
+    {code: 'products', title : 'Products', icon: Inventory},
+    {code: 'courses', title: 'Courses', icon: School},
+    {code: 'categories', title: 'Categories', icon: Category},
+    {code: 'organisations', title: 'Organisations', icon: Business},
+    {code: 'orders', title: 'Orders', icon: Shop2},
+]
+export default appbarMenus
